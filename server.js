82b7702fc280de3ce92c6a524c69158812ad790c
Sergei Druzhina <sdruzhin@deakin.edu.au>
2020-10-08 16:43:16 +1100

    Set up for api auth

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const Requester = require('./models/User');
 const mongoose = require('mongoose');
 const passport = require('passport')
 const passportLocalMongoose = require('passport-local-mongoose');
-const session = require('express-session');
+// const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
 const cors = require('cors');
 
 // Routes and API
 const requesterApi = require('./api/requester-api');
 //const workerApi = require('./api/worker-api');
-const authRoutes = require('./routes/auth-routes');
+const authApi = require('./api/auth-api');
 const config = require('./config');
 
 const app = express()
@@ -26,45 +26,27 @@ app.use(express.static('public'));
 app.use(cors());
 app.use(requesterApi);
 //app.use(workerApi);
-app.use('/auth', authRoutes);
+app.use('/auth', authApi);
 
 // Session and Passport
-app.use(session({
-    secret : 'Deakin2020',
-    resave: false,
-    saveUninitialized: false, 
-    cookie: { maxAge: 480000 },
-    store: new MongoStore({ 
-        mongooseConnection: mongoose.connection, 
-        ttl: 2 * 60 * 60 // 2 hours
-    })
-}));
+// app.use(session({
+//     secret : 'Deakin2020',
+//     resave: false,
+//     saveUninitialized: false, 
+//     cookie: { maxAge: 480000 },
+//     store: new MongoStore({ 
+//         mongooseConnection: mongoose.connection, 
+//         ttl: 2 * 60 * 60 // 2 hours
+//     })
+// }));
 
 app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.session());
 
 // Initialise Passport strategies for requesters
 passport.use(Requester.createStrategy())
 passport.serializeUser(Requester.serializeUser())
 passport.deserializeUser(Requester.deserializeUser())
-
-
-/**
- * Routes
- */
-
-// Entry point 
-app.get('/', (req, res) => {
-    res.json('Home');
-    // Check if the user is set in the session
-    // if (!req.isAuthenticated()) {
-    //     // redirect it to login page
-    //     // res.redirect('/auth/login');
-    // } 
-    // else {
-    //     // res.redirect('/reqtask');
-    // }
-})
 
 let port = process.env.PORT;
 if (port == null || port == '') {
@@ -72,5 +54,5 @@ if (port == null || port == '') {
 }
 
 app.listen(port, (req, res)=>{
-    console.log('Server is running successfully!');
+    console.log('Server is running successfully on port ' + port);
 })
