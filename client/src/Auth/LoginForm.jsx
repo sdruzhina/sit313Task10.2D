d17c5b80d992193ebfab4b1c188fa6e3c41d773f
Sergei Druzhina <sergei.druzhina@gmail.com>
2020-10-09 17:07:53 +1100

    Login working

--- client/src/Auth/LoginForm.jsx
+++ client/src/Auth/LoginForm.jsx
@@ -1,42 +1,96 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react';
+import { useHistory } from "react-router-dom";
 
-const LoginForm = () => (
-  <Grid textAlign='center' style={{ height: '80vh' }} verticalAlign='middle'>
-    <Grid.Column style={{ maxWidth: 450 }}>
-      <Header as='h2' color='blue' textAlign='center'>
-        iCrowdTask Login
-      </Header>
-      <Form size='large'>
-        <Segment stacked>
-        <Form.Input 
-            fluid 
-            icon='user' 
-            iconPosition='left' 
-            placeholder='Email address'
-            type='email'
-            name='email'
-            required 
-          />
-          <Form.Input
-            fluid
-            icon='lock'
-            iconPosition='left'
-            placeholder='Password'
-            type='password'
-            name='password'
-            required
-          />
-          <Button color='blue' fluid size='large'>
-            Login
-          </Button>
-        </Segment>
-      </Form>
-      <Message>
-        Don't have an account? <a href='/signup'>Sign Up</a>
-      </Message>
-    </Grid.Column>
-  </Grid>
-)
+function LoginForm() {
+    // Router history
+    const history = useHistory();
+
+  // Form data
+  const [userCredentials, setUserCredentials] = useState({
+    email: '',
+    password: ''
+  });
+  
+  // Event handler
+  const handleChange = (e, data) => {
+    const {name, value} = data;
+    setUserCredentials((data) => {
+      return {
+        ...data,
+        [name]: value
+      };
+    });
+  }
+  
+  const loginUser = async (e) => {
+    e.preventDefault();
+    const { email, password } = userCredentials;
+    if (email === '' || password === '') {
+      // this.setState({
+      //   showError: false,
+      //   showNullError: true,
+      //   loggedIn: false,
+      // });
+    } 
+    else {
+        fetch('http://localhost:8080/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(userCredentials)
+        })
+        .then(response => response.json())
+        .then((response) => {
+          localStorage.setItem('JWT', response.token);
+          history.push('/home');
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
+
+  return (
+    <Grid textAlign='center' style={{ height: '80vh' }} verticalAlign='middle'>
+      <Grid.Column style={{ maxWidth: 450 }}>
+        <Header as='h2' color='blue' textAlign='center'>
+          iCrowdTask Login
+        </Header>
+        <Form size='large' onSubmit={loginUser}>
+          <Segment stacked>
+          <Form.Input 
+              fluid 
+              icon='user' 
+              iconPosition='left' 
+              placeholder='Email address'
+              type='email'
+              name='email'
+              required
+              onChange={handleChange} 
+            />
+            <Form.Input
+              fluid
+              icon='lock'
+              iconPosition='left'
+              placeholder='Password'
+              type='password'
+              name='password'
+              required
+              onChange={handleChange}
+            />
+            <Button color='blue' fluid size='large' type='submit'>
+              Login
+            </Button>
+          </Segment>
+        </Form>
+        <Message>
+          Don't have an account? <a href='/signup'>Sign Up</a>
+        </Message>
+      </Grid.Column>
+    </Grid>
+  );
+}
 
 export default LoginForm;
--- client/src/Auth/SignupForm.jsx
+++ client/src/Auth/SignupForm.jsx
@@ -2,80 +2,82 @@ import React from 'react';
 import { Button, Form, Checkbox, Grid, Header, Message, Segment } from 'semantic-ui-react';
 import './SignupForm.css';
 
-const SignupForm = () => (
-  <Grid textAlign='center' style={{ height: '80vh' }} verticalAlign='middle'>
-    <Grid.Column style={{ maxWidth: 450 }}>
-      <Header as='h2' color='blue' textAlign='center'>
-        Create iCrowdTask Account
-      </Header>
-      <Form size='large'>
-        <Segment stacked>
-        <Form.Input 
-            fluid 
-            placeholder='First name'
-            type='text'
-            name='firstName' 
-            required
-          />
+function SignupForm() {
+  
+  return (
+    <Grid textAlign='center' style={{ height: '80vh' }} verticalAlign='middle'>
+      <Grid.Column style={{ maxWidth: 450 }}>
+        <Header as='h2' color='blue' textAlign='center'>
+          Create iCrowdTask Account
+        </Header>
+        <Form size='large'>
+          <Segment stacked>
           <Form.Input 
-            fluid 
-            placeholder='Last name'
-            type='text'
-            name='lastName' 
-            required
-          />
-          <Form.Input 
-            fluid 
-            placeholder='Email address'
-            type='email'
-            name='email'
-            required 
-          />
-          <Form.Input
-            fluid
-            placeholder='Password'
-            type='password'
-            name='password'
-            required
-          />
-          <Form.Input
-            fluid
-            placeholder='Password confirmation'
-            type='password'
-            name='passwordConfirm'
-            required
-          />
-          <div className='form-flex'>
-            <Form.Field>
-              Sign up as: 
-            </Form.Field>
-            <Form.Field>
-              <Checkbox
-                className='flex-item'
-                label='Requester'
-                name='isRequester'
-                value={true}
-              />
-            </Form.Field>
-            <Form.Field>
-              <Checkbox
-                className='flex-item'
-                label='Worker'
-                name='isWorker'
-                value={false}
-              />
-            </Form.Field>
-          </div>
-          <Button color='blue' fluid size='large'>
-            Create Account
-          </Button>
-        </Segment>
-      </Form>
-      <Message>
-        Have an account? <a href='/login'>Log in</a>
-      </Message>
-    </Grid.Column>
-  </Grid>
-)
-
+              fluid 
+              placeholder='First name'
+              type='text'
+              name='firstName' 
+              required
+            />
+            <Form.Input 
+              fluid 
+              placeholder='Last name'
+              type='text'
+              name='lastName' 
+              required
+            />
+            <Form.Input 
+              fluid 
+              placeholder='Email address'
+              type='email'
+              name='email'
+              required 
+            />
+            <Form.Input
+              fluid
+              placeholder='Password'
+              type='password'
+              name='password'
+              required
+            />
+            <Form.Input
+              fluid
+              placeholder='Password confirmation'
+              type='password'
+              name='passwordConfirm'
+              required
+            />
+            <div className='form-flex'>
+              <Form.Field>
+                Sign up as: 
+              </Form.Field>
+              <Form.Field>
+                <Checkbox
+                  className='flex-item'
+                  label='Requester'
+                  name='isRequester'
+                  value={true}
+                />
+              </Form.Field>
+              <Form.Field>
+                <Checkbox
+                  className='flex-item'
+                  label='Worker'
+                  name='isWorker'
+                  value={false}
+                />
+              </Form.Field>
+            </div>
+            <Button color='blue' fluid size='large'>
+              Create Account
+            </Button>
+          </Segment>
+        </Form>
+        <Message>
+          Have an account? <a href='/login'>Log in</a>
+        </Message>
+      </Grid.Column>
+    </Grid>
+  );
+}
 export default SignupForm;
