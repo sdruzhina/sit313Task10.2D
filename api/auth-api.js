dd660a4669e81ffe29086953b3c47223a6adf250
Sergei Druzhina <sergei.druzhina@gmail.com>
2020-10-09 15:54:34 +1100

    Added JWT strategy

--- api/auth-api.js
+++ api/auth-api.js
@@ -6,7 +6,7 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 //const mail = require('../mail');
 
-// Register a new user API route
+// Register a new user API route with a custom local strategy
 router.post('/register', (req, res, next) => {
     if (req.body.password !== req.body.passwordConfirm) {
         res.status(401).send({ message: 'Password and password confirmation must match' });
@@ -29,7 +29,7 @@ router.post('/register', (req, res, next) => {
     })(req, res, next);
   });
 
-// Local strategy login
+// Login with local strategy
 router.post('/login', (req, res, next) => {
     passport.authenticate('local', (err, users, info) => {
         if (err) {
@@ -57,40 +57,40 @@ router.post('/login', (req, res, next) => {
     })(req, res, next);
 });
 
-// Signup page
-router.get('/reqsignup', (req, res) => {
-    // res.render('reqsignup.ejs', { err: [], data: null });
-})
-
-// Signup form
-router.post('/reqsignup', (req, res) => {
-    if (req.body.password !== req.body.passwordConfirm) {
-        // res.render('reqsignup.ejs', { err: { errors: {passwordConfirm: 'Passwords must match.'}}, data: req.body })
-    }
-    User.register(new User({
-        country: req.body.country,
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        address1: req.body.address1,
-        address2: req.body.address2,
-        city: req.body.city,
-        state: req.body.state,
-        postcode: req.body.postcode,
-        mobile: req.body.mobile,
-        createdAt: Date.now()
-    }), req.body.password, (err, user) => {
+// TEST API secure endpoint
+router.get('/user', (req, res, next) => {
+    passport.authenticate('jwt', { session: false }, (err, user, info) => {
         if (err) {
             console.log(err);
-            // res.render('reqsignup.ejs', { err: err, data: req.body });
         }
-        else {
-            passport.authenticate('local')(req, res, () => {
-                // res.redirect('/reqtask');
+        if (info !== undefined) {
+            console.log(info.message);
+            res.status(401).send(info.message);
+        } 
+        else if (user.email === req.query.email) {
+            User.findOne({ email: req.query.email })
+            .then((userInfo) => {
+            if (userInfo != null) {
+                console.log('User found in db');
+                res.status(200).send({
+                    auth: true,
+                    firstName: userInfo.firstName,
+                    lastName: userInfo.lastName,
+                    email: userInfo.email,
+                    message: 'User found in db',
+                });
+            } 
+            else {
+                console.error('No user exists in db with that email');
+                res.status(401).send('No user exists in db with that email');
+            }
             });
+        } else {
+            console.error('JWT id and email do not match');
+            res.status(403).send('JWT id and email do not match');
         }
-    })
-});
+        })(req, res, next);
+  });
 
 // Logout
 router.get('/logout', (req, res) => {
@@ -98,95 +98,5 @@ router.get('/logout', (req, res) => {
     // res.redirect('/');
 });
 
-// Password reset form
-router.get('/forgot', (req, res) => {
-    // res.render('forgot-password', { sent: false });
-});
-
-// Password Reset POST request
-router.post('/forgot', async function (req, res) {
-    // Get the email and check if user exists
-    const email = req.body.email.trim();
-    let user;
-    try {
-        user = await User.findOne({ email }).exec()
-    } 
-    catch (err) {
-        res.status(404).json('This email is not registered.')
-    }
-
-    // Create a JWT token using the constant secret
-    const userId = user._id;
-    const token = jwt.sign({ userId }, process.env.JWT_SECRET || config.keys.jwtSecret, {
-        expiresIn: 1800 // 30 minutes
-    });
-
-    // Save the token to user's DB record
-    user.updateOne({ resetToken: token }, (err, successs) => {
-        if (err) {
-            res.status(404).json('Error saving token.')
-        }
-    });
-
-    // Password reset URL using user ID and token
-    const url = `https://ancient-chamber-36974.herokuapp.com/auth/reset/${token}`;
-
-    // Send the email
-    //mail.sendPasswordReset(user.email, user.firstName, url);
-
-    // Render the page with a confirmation message
-    // res.render('forgot-password', { sent: true });
-});
-
-// Change Password form
-router.get('/reset/:token', (req, res) => {
-    // res.render('change-password', 
-    //     { err: null, confirm: false, token: req.params.token });
-});
-
-// Change Password POST request
-router.post('/reset/:token', async function (req, res) {
-    if (!req.body.password || req.body.password !== req.body.passwordConfirm) {
-        // res.render('change-password', 
-        //     { err: {message: 'Passwords do not match.'}, 
-        //     confirm: false, token: req.params.token });
-    }
-    // Decode the token
-    let decoded;
-    try {
-        decoded = jwt.verify(req.params.token, process.env.JWT_SECRET || config.keys.jwtSecret);
-    } 
-    catch (err) {
-        console.log(err);
-        res.status(403).json('Token verification failed.');
-    }
-    // Find the user by ID stored in token
-    let user;
-    try {
-        user = await User.findOne({ _id: decoded.userId }).exec();
-    } 
-    catch (err) {
-        res.status(404).json('Invalid user.');
-    }
-    // Compare tokens
-    if (user && user.resetToken === req.params.token) {
-        // Set new password 
-        await user.setPassword(req.body.password);
-        await user.save();
-
-        // Remove the token from user's DB record
-        user.updateOne({ resetToken: '' }, (err, success) => {
-            if (err) {
-                res.status(404).json('Error resetting token.')
-            }
-        });
-
-        // Confirm password change
-        res.render('change-password', { err: null, confirm: true, token: '' });
-    }
-    else {
-        res.status(404).json('User not found, or the token is invalid.')
-    }
-});
 
 module.exports = router;
--- config/passport.js
+++ config/passport.js
@@ -56,62 +56,30 @@ passport.use(
   ),
 );
 
-// Local strategy for user login
-// passport.use(
-//     'login',
-//     new LocalStrategy(
-//     {
-//         usernameField: 'email',
-//         passwordField: 'password',
-//         session: false,
-//     },
-//     (email, password, done) => {
-//         try {
-//             User.findOne({ email })
-//             .then(user => {
-//             if (user === null) {
-//                 return done(null, false, { message: 'User not found' });
-//             }
-//             bcrypt.compare(password, user.password).then(response => {
-//             if (response !== true) {
-//                 console.log('passwords do not match');
-//                 return done(null, false, { message: 'passwords do not match' });
-//             }
-//             console.log('user found & authenticated');
-//             return done(null, user);
-//             });
-//         });
-//         } catch (err) {
-//         done(err);
-//         }
-//     },
-//     ),
-//     );
+// JWT strategy for API authorisation once user is logged in
+const opts = {
+  jwtFromRequest: ExtractJWT.fromAuthHeaderWithScheme('JWT'),
+  secretOrKey: config.keys.jwtSecret,
+};
 
-// const opts = {
-//   jwtFromRequest: ExtractJWT.fromAuthHeaderWithScheme('JWT'),
-//   secretOrKey: config.keys.jwtSecret,
-// };
-
-// passport.use(
-//   'jwt',
-//   new JWTstrategy(opts, (jwt_payload, done) => {
-//     try {
-//       User.findOne({
-//         where: {
-//           id: jwt_payload.id,
-//         },
-//       }).then(user => {
-//         if (user) {
-//           console.log('user found in db in passport');
-//           done(null, user);
-//         } else {
-//           console.log('user not found in db');
-//           done(null, false);
-//         }
-//       });
-//     } catch (err) {
-//       done(err);
-//     }
-//   }),
-// );
+passport.use(
+    'jwt',
+    new JWTstrategy(opts, (jwt_payload, done) => {
+        try {
+            User.findById(jwt_payload.id)
+            .then(user => {
+                if (user) {
+                    console.log('User found');
+                    done(null, user);
+                } 
+                else {
+                    console.log('User not found');
+                    done(null, false);
+                }
+            });
+        } 
+        catch (err) {
+            done(err);
+        }
+    }),
+);
