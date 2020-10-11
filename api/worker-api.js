6278f613869e86c9c4f959970298c00b3c6fa124
Sergei Druzhina <sergei.druzhina@gmail.com>
2020-10-11 12:19:02 +1100

    Worker task lookup query created

--- api/worker-api.js
+++ api/worker-api.js
@@ -5,16 +5,47 @@ const User = require('../models/User');
 const Task = require('../models/Task');
 const WorkerTask = require('../models/WorkerTask');
 
-// Get all tasks
-router.get('/worker/tasks', (req, res) => {
-    Task.find((err, taskList) => {
-        if (err) {
-            res.json(err);
-        }
-        else {
-            res.json(taskList);
-        }
-    });
+// Get all tasks available to the worker
+router.get('/worker/tasks', (req, res, next) => {
+            // Get tasks belonging to the authenticated user
+            Task.aggregate([
+                {$match: {'status': 'AVAILABLE'} },
+                {
+                    $lookup:
+                    {
+                        from: 'WorkerTask',
+                        localField: '_id',
+                        foreignField: 'taksId',
+                        as: 'workerTask'
+                    }
+                }], (err, taskList) => {
+                    if (err) {
+                        res.json(err);
+                    }
+                    else {
+                        res.json(taskList);
+                    }
+            });
+    // passport.authenticate('jwt', { session: false }, (err, user, info) => {
+    //     if (err) {
+    //         console.log(err);
+    //     }
+    //     if (info !== undefined) {
+    //         console.log(info.message);
+    //         res.status(401).send(info.message);
+    //     } 
+    //     else {
+    //         // Get tasks available to the authenticated user
+    //         Task.find({ status: 'AVAILABLE' }, (err, taskList) => {
+    //             if (err) {
+    //                 res.json(err);
+    //             }
+    //             else {
+    //                 res.json(taskList);
+    //             }
+    //         });
+    //     }
+    // })(req, res, next);
 });
 
 
